d7886c87598d22eeee62bdee37cbb7199db2619f
huyhao27 <89589680+huyhao27@users.noreply.github.com>
2024-08-20 17:54:32 +0700

    Create menu.java
    dmcuoidoi

---
+++ src/assignment_pro/Menu.java
@@ -0,0 +1,39 @@
+package assignment_pro;
+
+import java.util.Scanner;
+
+public class Menu {
+    private String[] menuItems;
+    private int count = 0;
+    private int size;
+
+    public Menu(int size) {
+        if (size < 1) {
+            size = 10;
+        }
+        this.size = size;
+        this.menuItems = new String[this.size];
+    }
+
+    public void addMenuItem(String menuItem) {
+        if (this.count < this.size) {
+            menuItems[count] = menuItem;
+            count++;
+        }
+    }
+
+    public int getChoice() {
+        int choice = 0;
+        if (count > 0) {
+            for (int i = 0; i < count; i++) {
+                System.out.println((i + 1) + ". " + menuItems[i]);
+            }
+        }
+        System.out.print("Please select an option: ");
+        Scanner sc = new Scanner(System.in);
+        if (sc.hasNextInt()) {
+            choice = sc.nextInt();
+        }
+        return choice;
+    }
+}
