9a9cd9b786a475780a045881ad4ff4593dfa3b1f
congquynh0206 <congquynh0206@gmail.com>
2024-08-20 17:20:49 +0700

    q
    q

--- src/assignment_pro/Member.java
+++ src/assignment_pro/Member.java
@@ -1,17 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package assignment_pro;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/**
- *
- * @author ADMIN
- */
-// Class để lưu trữ thông tin thành viên
 public class Member {
+
     static ArrayList<Member> members = new ArrayList<>();
     String name;
     String position;
@@ -45,7 +37,7 @@ public class Member {
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
-    
+
     public Member(String name, String ID, String position, String phoneNumber) {
         this.name = name;
         this.position = position;
@@ -55,31 +47,61 @@ public class Member {
 
     @Override
     public String toString() {
-        return "Rollnumber: "+ ID+ ", Tên: " + name + ", Vị trí: " + position + ", Số điện thoại: " + phoneNumber;
+        return "Rollnumber: " + ID + ", Tên: " + name + ", Vị trí: " + position + ", Số điện thoại: " + phoneNumber;
     }// Thêm thành viên mới
+
     public void addMember() {
-        while(true){
-        System.out.print("Nhập tên thành viên: ");
-        String name = scanner.nextLine();
-        System.out.print("Enter rollnumber: ");
-        String ID = scanner.nextLine();
-        System.out.print("Nhập vị trí: ");
-        String position = scanner.nextLine();
-        System.out.print("Nhập số điện thoại: ");
-        String phoneNumber = scanner.nextLine();
-        
-        if(isExist(ID, phoneNumber)){
-            System.out.println("Thông tin trùng (mssv or sdt), vui lòng nhập lại");     
-            continue;
-        }
-        System.out.println("Thành viên đã được thêm thành công.");
-        members.add(new Member(name,ID, position, phoneNumber));
-        break;
+        while (true) {
+            System.out.print("Nhập tên thành viên: ");
+            String name = scanner.nextLine();
+            System.out.print("Enter rollnumber: ");
+            String ID = scanner.nextLine();
+            System.out.print("Nhập vị trí: ");
+            String position = scanner.nextLine();
+
+            String phoneNumber;
+            do {
+                System.out.print("Nhập số điện thoại: ");
+                phoneNumber = scanner.nextLine();
+                if (!checkPhoneNumber(phoneNumber)) {
+                    System.out.println("SDT chỉ bao gồm các chữ số. Vui lòng nhập lại...");
+                }
+            } while (checkPhoneNumber(phoneNumber) == false);
+
+            if (isExist(ID, phoneNumber)) {
+                System.out.println("Thông tin trùng (mssv or sdt), vui lòng nhập lại");
+                continue;
+            }
+            System.out.println("Thành viên đã được thêm thành công.");
+            members.add(new Member(formatName(name), ID, position, phoneNumber));
+            break;
         }
     }
 
+    public static String formatName(String name) {
+
+        name = name.trim().toLowerCase();
+
+        String[] words = name.split("\\s+");
+        StringBuilder formattedName = new StringBuilder();
+
+        for (String word : words) {
+            if (!word.isEmpty()) {
+                formattedName.append(Character.toUpperCase(word.charAt(0)))
+                        .append(word.substring(1))
+                        .append(" ");
+            }
+        }
+
+        return formattedName.toString().trim();
+    }
+
+    public boolean checkPhoneNumber(String phone_Num) {
+        return phone_Num.matches("\\d+");
+    }
+
     // Xóa thành viên
-   /* public void removeMemberByName() {
+    /* public void removeMemberByName() {
         System.out.print("Nhập tên thành viên cần xóa: ");
         String name = scanner.nextLine();
         
@@ -101,7 +123,7 @@ public class Member {
     public void removeMemberByID() {
         System.out.print("Enter rollnumber want to remove: ");
         String IDremove = scanner.nextLine();
-        
+
         Member memberToRemove = null;
         for (Member member : members) {
             if (member.ID.equalsIgnoreCase(IDremove)) {
@@ -122,26 +144,28 @@ public class Member {
     public void updateMember() {
         System.out.print("Nhập mssv thành viên cần cập nhật: ");
         String findID = scanner.nextLine();
-        boolean isWrong= true;
-        
+        boolean isWrong = true;
+
         for (Member member : members) {
-            while(isWrong){
+            while (isWrong) {
                 isWrong = false;
                 if (member.ID.equalsIgnoreCase(findID)) {
                     System.out.print("Nhập vị trí mới: ");
-                    member.setPosition(scanner.nextLine()) ;
+                    member.setPosition(scanner.nextLine());
                     System.out.print("Nhập số điện thoại mới: ");
                     String newPhone = scanner.nextLine();
-                    member.setPhoneNumber(newPhone) ;
-                    for(Member memberCheckPhone : members){
-                        if(!memberCheckPhone.ID.equalsIgnoreCase(findID) && newPhone.equals(memberCheckPhone.getPhoneNumber())){
+                    member.setPhoneNumber(newPhone);
+                    for (Member memberCheckPhone : members) {
+                        if (!memberCheckPhone.ID.equalsIgnoreCase(findID) 
+                            && newPhone.equals(memberCheckPhone.getPhoneNumber())) {
                             System.out.println("SDT da ton tai, vui long nhap lai");
                             isWrong = true;
                             break;
                         }
-                    }       
+                    }
+                } else {
+                    System.out.println("Không tìm thấy thành viên.");
                 }
-                else System.out.println("Không tìm thấy thành viên.");
             }
         }
         System.out.println("Thông tin thành viên đã được cập nhật.");
@@ -159,7 +183,8 @@ public class Member {
             }
         }
     }
-    public void findByID(String findID){
+
+    public void findByID(String findID) {
         for (Member member : members) {
             if (member.ID.equalsIgnoreCase(findID)) {
                 System.out.println(member);
@@ -168,13 +193,14 @@ public class Member {
         }
         System.out.println("Không tìm thấy thành viên.");
     }
-    public boolean isExist(String checkID, String checkPhone ){
-        for(Member member : members){
-            if(member.ID.equalsIgnoreCase(checkID) || member.phoneNumber.equals(checkPhone)){
+
+    public boolean isExist(String checkID, String checkPhone) {
+        for (Member member : members) {
+            if (member.ID.equalsIgnoreCase(checkID) 
+                || member.phoneNumber.equals(checkPhone)) {
                 return true;
             }
         }
         return false;
     }
 }
-
